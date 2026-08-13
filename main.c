f3e792dcf15f0281d92e4d71ef52ca7f5b36248b
Bruno Assenço <brunoass.acounts@gmail.com>
2026-08-13 19:22:29 -0300

    Modelando codigo e criando funcao de verificar lista do estoque

--- main.c
+++ main.c
@@ -7,89 +7,110 @@ int main(void) {
 
     //Tela inicial
 
+    int quantidade[MAX]={0};
+    char produto[MAX][100]={};
+    float preco[MAX]={0};
+    int codigo[MAX]={0};
     int funcao;
     int entradaValida;
+    int novoProduto=0;
 
-    do {                                                    //Cria o loop caso receba a entrada invalida
-        printf("1-Cadastrar um novo produto\n");
-        printf("2-Consultar produtos em estoque\n");
-        printf("3-Alterar produtos em estoque\n");
-        entradaValida = scanf("%d", &funcao);
-        if (entradaValida!=1 || funcao!=1 && funcao!=2 && funcao!=3) {    //Verifica se a entrada é valida
-            while (getchar() != '\n');      //Limpa o buffer
-        }
-    }while (entradaValida!=1 || funcao!=1 && funcao!=2 && funcao!=3);
+    do{
 
-    switch (funcao) {
-
-        case 1: {
-            //Cadastrar um novo produto
-
-            int quantidade[MAX];
-            char produto[MAX][100];
-            float preco[MAX];
-            int codigo[MAX];
-            char opcao='s';
-
-            for (int i=0; opcao!='n' && i<MAX; i++){
-
-                //Inserir nome do produto
-
-                do {
-                    printf("Produto:\n");
-                    entradaValida = scanf("%99s", produto[i]); //Limita a entrada de uma string menor que 100 caracteres
-                    if (entradaValida!=1) {
-                        while (getchar() != '\n');
-                    }
-                }while (entradaValida!=1);
-
-                //Inserir codigo do produto
-
-                do {                                            //Cria um loop ate o usuario digitar uma entrada valida
-                    printf("Codigo:\n");
-                    entradaValida = scanf("%d", &codigo[i]);
-                    if (entradaValida!=1 || codigo[i]<0) {      //Verifica se o usuario digitou uma entreda valida
-                        while (getchar() != '\n');              //Limpa o buffer
-                    }
-                }while (entradaValida!=1 || codigo[i]<0);
-
-                //Inserir quantidade
-
-                do {
-                    printf("Quantidade a ser armazenada:\n");   //Mesmos comentarios da entrada de codigo se aplicam
-                    entradaValida = scanf("%d", &quantidade[i]);
-                    if (entradaValida!=1 || quantidade[i]<1) {
-                        while (getchar() != '\n');
-                    }
-                }while (entradaValida!=1 || quantidade[i]<1);
-
-                //Inserir preco
-
-                do {
-                    printf("Preco do produto:\n");              //Mesmos comentarios da entrada de codigo se aplicam
-                    entradaValida = scanf("%f", &preco[i]);
-                    if (entradaValida!=1 || preco[i]<0.5) {
-                        while (getchar() != '\n');
-                    }
-                }while (entradaValida!=1 || preco[i]<0.5);
-
-                //Mostra todos os dados do produto
-                printf("\t%s\n", produto[i]);
-                printf("\tCodigo: %d\n", codigo[i]);
-                printf("\tPreco: %.2f\n", preco[i]);
-                printf("\tQuantidade: %d\n\n", quantidade[i]);
-
-                do {
-                    printf("Deseja adicionar mais algum?  (s/n)");
-                    scanf(" %c", &opcao);
-                    if (opcao!='s' && opcao!='n') {
-                        while (getchar() != '\n');
-                    }
-                }while (opcao!='s' && opcao!='n');
+        do {                                                    //Cria o loop caso receba a entrada invalida
+            printf("1-Cadastrar um novo produto\n");
+            printf("2-Consultar produtos em estoque\n");
+            printf("3-Alterar produtos em estoque\n");
+            entradaValida = scanf("%d", &funcao);
+            if (entradaValida!=1 || funcao!=1 && funcao!=2 && funcao!=3) {    //Verifica se a entrada é valida
+                printf("Entrada Invalida!\n");
+                while (getchar() != '\n');      //Limpa o buffer
             }
-            break;
-        }
+        }while (entradaValida!=1 || funcao!=1 && funcao!=2 && funcao!=3);
 
-    }
-    return 0;
+        switch (funcao) {
+
+            case 1: {
+                //Cadastrar um novo produto
+
+                char opcao='s';
+
+                for (int i=0; opcao!='n' && i<MAX; i++){
+
+                    //Inserir nome do produto
+
+                    do {
+                        printf("Produto:\n");
+                        entradaValida = scanf(" %99[^\n]", produto[novoProduto]); //Limita a entrada de uma string menor que 100 caracteres
+                        if (entradaValida!=1) {
+                            printf("Entrada Invalida!\n");
+                            while (getchar() != '\n');
+                        }
+                    }while (entradaValida!=1);
+
+                    //Inserir codigo do produto
+
+                    codigo[novoProduto]=novoProduto+1;
+
+                    //Inserir quantidade
+
+                    do {                                            //Cria um loop ate o usuario digitar uma entrada valida
+                        printf("Quantidade a ser armazenada:\n");
+                        entradaValida = scanf("%d", &quantidade[novoProduto]);
+                        if (entradaValida!=1 || quantidade[novoProduto]<1) {        //Verifica se o usuario digitou uma entrada valida
+                            printf("Entrada Invalida!\n");
+                            while (getchar() != '\n');                              //Limpa o buffer
+                        }
+                    }while (entradaValida!=1 || quantidade[novoProduto]<1);
+
+                    //Inserir preco
+
+                    do {
+                        printf("Preco do produto:\n");              //Mesmos comentarios da entrada de codigo se aplicam
+                        entradaValida = scanf("%f", &preco[novoProduto]);
+                        if (entradaValida!=1 || preco[novoProduto]<0.5) {
+                            printf("Entrada Invalida!\n");
+                            while (getchar() != '\n');
+                        }
+                    }while (entradaValida!=1 || preco[novoProduto]<0.5);
+
+                    //Mostra todos os dados do produto
+                    printf("\tCodigo: %03d\n", codigo[novoProduto]);
+                    printf("\t%s\n", produto[novoProduto]);
+                    printf("\tQuantidade: %d\n", quantidade[novoProduto]);
+                    printf("\tPreco: R$%.2f\n", preco[novoProduto]);
+
+                    novoProduto++;
+
+                    do {
+                        printf("Deseja adicionar mais algum?  (s/n)");
+                        scanf(" %c", &opcao);
+                        if (opcao!='s' && opcao!='n') {
+                            printf("Entrada Invalida!\n");
+                            while (getchar() != '\n');
+                        }
+                    }while (opcao!='s' && opcao!='n');
+                }
+                break;
+            }
+
+            case 2: {
+                //Consultar produtos em estoque
+
+                for (int i=0; i<MAX; i++){      //Imprime a lista completa
+
+                    if (codigo[i]<1) break;     //Imprime somente a parte da lista que contem valores
+
+                    printf("%03d - ", codigo[i]);
+                    printf("%s | ", produto[i]);
+                    printf("Quantidade: %d | ", quantidade[i]);
+                    printf("Preco: R$%.2f\n", preco[i]);
+                }
+
+            }
+
+        }
+    }while(1);      //Faz programa rodar infinitamente
+
+return 0;
 }
