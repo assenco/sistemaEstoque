b5044e8cb5cfac68cad7e6d0a6e86a46baf46c97
Bruno Assenço <brunoass.acounts@gmail.com>
2026-08-15 11:23:18 -0300

    Criando funcao de editar itens

--- main.c
+++ main.c
@@ -14,7 +14,6 @@ int main(void) {
     int codigo[MAX]={0};
     int funcao;
     int entradaValida;
-    int novoProduto=0;
 
     do{
 
@@ -35,14 +34,17 @@ int main(void) {
                 //Cadastrar um novo produto
 
                 char opcao='s';
+                int n=0;
 
                 for (int i=0; opcao!='n' && i<MAX; i++){
 
                     //Inserir nome do produto
 
+                    for (n ;codigo[n]!=0; n++);
+
                     do {
                         printf("Produto:\n");
-                        entradaValida = scanf(" %99[^\n]", produto[novoProduto]); //Limita a entrada de uma string menor que 100 caracteres
+                        entradaValida = scanf(" %99[^\n]", produto[n]); //Limita a entrada de uma string menor que 100 caracteres
                         if (entradaValida!=1) {
                             printf("Entrada Invalida!\n");
                             while (getchar() != '\n');
@@ -51,37 +53,35 @@ int main(void) {
 
                     //Inserir codigo do produto
 
-                    codigo[novoProduto]=novoProduto+1;
+                    codigo[n]=n+1;
 
                     //Inserir quantidade
 
                     do {                                            //Cria um loop ate o usuario digitar uma entrada valida
                         printf("Quantidade a ser armazenada:\n");
-                        entradaValida = scanf("%d", &quantidade[novoProduto]);
-                        if (entradaValida!=1 || quantidade[novoProduto]<1) {        //Verifica se o usuario digitou uma entrada valida
+                        entradaValida = scanf("%d", &quantidade[n]);
+                        if (entradaValida!=1 || quantidade[n]<1) {        //Verifica se o usuario digitou uma entrada valida
                             printf("Entrada Invalida!\n");
                             while (getchar() != '\n');                              //Limpa o buffer
                         }
-                    }while (entradaValida!=1 || quantidade[novoProduto]<1);
+                    }while (entradaValida!=1 || quantidade[n]<1);
 
                     //Inserir preco
 
                     do {
                         printf("Preco do produto:\n");              //Mesmos comentarios da entrada de codigo se aplicam
-                        entradaValida = scanf("%f", &preco[novoProduto]);
-                        if (entradaValida!=1 || preco[novoProduto]<0.5) {
+                        entradaValida = scanf("%f", &preco[n]);
+                        if (entradaValida!=1 || preco[n]<0.5) {
                             printf("Entrada Invalida!\n");
                             while (getchar() != '\n');
                         }
-                    }while (entradaValida!=1 || preco[novoProduto]<0.5);
+                    }while (entradaValida!=1 || preco[n]<0.5);
 
                     //Mostra todos os dados do produto
-                    printf("\tCodigo: %03d\n", codigo[novoProduto]);
-                    printf("\t%s\n", produto[novoProduto]);
-                    printf("\tQuantidade: %d\n", quantidade[novoProduto]);
-                    printf("\tPreco: R$%.2f\n", preco[novoProduto]);
-
-                    novoProduto++;
+                    printf("\tCodigo: %03d\n", codigo[n]);
+                    printf("\t%s\n", produto[n]);
+                    printf("\tQuantidade: %d\n", quantidade[n]);
+                    printf("\tPreco: R$%.2f\n", preco[n]);
 
                     do {
                         printf("Deseja adicionar mais algum?  (s/n)");
@@ -98,7 +98,7 @@ int main(void) {
             case 2: {
                 //Consultar produtos em estoque
 
-                for (int i=0; i<novoProduto; i++){      //Imprime a lista completa
+                for (int i=0; i<MAX; i++){      //Imprime a lista completa
 
                     if (codigo[i]<1) continue;     //Imprime somente a parte da lista que contem valores
 
@@ -113,25 +113,75 @@ int main(void) {
             case 3: {
 
                 int opcao;
-                int produtoDeletar;
+                int produtoEditar;
+
+                printf("Qual o codigo do produto?\n");
+                scanf("%d", &produtoEditar);
+
+                printf("%03d - ", codigo[produtoEditar-1]);
+                printf("%s | ", produto[produtoEditar-1]);
+                printf("Quantidade: %d | ", quantidade[produtoEditar-1]);
+                printf("Preco: R$%.2f\n", preco[produtoEditar-1]);
 
                 printf("1-Apagar item\n");
                 printf("2-Editar item\n");
                 scanf("%d", &opcao);
 
-                printf("Qual o codigo do produto?\n");
-                scanf("%d", &produtoDeletar);
-
                 //Apagar item do estoque
 
                 if (opcao==1) {
-                    strcpy(produto[produtoDeletar-1], "");
-                    codigo[produtoDeletar-1]=0;
-                    quantidade[produtoDeletar-1]=0;
-                    preco[produtoDeletar-1]=0;
+                    strcpy(produto[produtoEditar-1], "");
+                    codigo[produtoEditar-1]=0;
+                    quantidade[produtoEditar-1]=0;
+                    preco[produtoEditar-1]=0;
 
                     printf("Produto deletado!\n");
+
+                    break;
                 }
+
+                //Editar item do estoque
+
+                if (opcao==2) {
+
+                    int itemModificar;
+                    char novaModificacao;
+
+                    do {
+                        printf("Qual informacao deseja editar:\n");
+
+                        printf("1-Produto\n");
+                        printf("2-Quantidade\n");
+                        printf("3-Preco\n");
+
+                        scanf("%d", &itemModificar);
+
+                        if (itemModificar==1) {
+                            printf("Digite o produto:\n");
+                            strcpy("%s", produto[produtoEditar-1]);
+                        }
+
+                        if (itemModificar==2) {
+                            printf("Digite a quantidade:\n");
+                            scanf("%d", &quantidade[produtoEditar-1]);
+                        }
+
+                        if (itemModificar==3) {
+                            printf("Digite o preco:\n");
+                            scanf("%f", &preco[produtoEditar-1]);
+                        }
+
+                        printf("Item modificado!\n");
+
+                        printf("Deseja modificar mais algum item deste produto?\n");
+                        scanf("%s", novaModificacao);
+
+                    }while (novaModificacao=='s');      //Revisar loop
+
+                    break;
+
+                }
+
                 break;
             }
         }
